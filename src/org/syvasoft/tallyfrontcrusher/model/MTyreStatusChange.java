f1df66e77111237992979631404ba67b547c6d51
Syed <Syed@localhost>
2017-07-31 21:07:29 +0530

    Tyre Status/Type Change document completed.

--- src/org/syvasoft/tallyfrontcrusher/model/MTyreStatusChange.java
+++ src/org/syvasoft/tallyfrontcrusher/model/MTyreStatusChange.java
@@ -3,6 +3,9 @@ package org.syvasoft.tallyfrontcrusher.model;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+import org.compiere.process.DocAction;
+import org.compiere.util.DB;
+
 public class MTyreStatusChange extends X_TF_TyreStatusChange {
 
 	public MTyreStatusChange(Properties ctx, int TF_TyreStatusChange_ID,
@@ -20,5 +23,37 @@ public class MTyreStatusChange extends X_TF_TyreStatusChange {
 	 * 
 	 */
 	private static final long serialVersionUID = 8261510390583803977L;
-
+	
+	private boolean isLatestDocument() {
+		String sql = " SELECT COUNT(*) FROM TF_TyreStatusChange WHERE TF_Tyre_ID = ? AND DateAcct > ? ";
+		int count = DB.getSQLValue(get_TrxName(), sql, getTF_Tyre_ID(), getDateAcct());
+		return (count == 0);
+	}
+	public void processIt(String docAction) {
+		if(DocAction.ACTION_Prepare.equals(docAction)) {
+			setDocStatus(DOCSTATUS_InProgress);
+		}
+		else if(DocAction.ACTION_Complete.equals(docAction)) {
+			setDocStatus(DOCSTATUS_Completed);
+			setProcessed(true);
+			if(isLatestDocument()) {
+				MTyre tyre = new MTyre(getCtx(), getTF_Tyre_ID(), get_TrxName());
+				tyre.setTF_TyreStatus_ID(getNew_TF_TyreStatus_ID());
+				if(getNew_TF_TyreType_ID() > 0)
+					tyre.setCurrent_TyreType_ID(getNew_TF_TyreType_ID());
+				tyre.saveEx();
+			}
+		}
+	}
+	public void reverseIt() {
+		setProcessed(false);
+		setDocStatus(DOCSTATUS_Drafted);
+		
+		if(isLatestDocument()) {
+			MTyre tyre = new MTyre(getCtx(), getTF_Tyre_ID(), get_TrxName());
+			tyre.setTF_TyreStatus_ID(getCurr_TF_TyreStatus_ID());
+			tyre.setCurrent_TyreType_ID(getCurr_TF_TyreType_ID());
+			tyre.saveEx();
+		}
+	}
 }
