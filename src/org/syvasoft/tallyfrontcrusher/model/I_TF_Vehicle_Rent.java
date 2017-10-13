097d8b59abdc85d3f954c7a04ac930777086032e
Syed <Syed@localhost>
2017-10-13 20:50:36 +0530

    To support existing Vehicle Rent document which will be removed eventually.

---
+++ src/org/syvasoft/tallyfrontcrusher/model/I_TF_Vehicle_Rent.java
@@ -0,0 +1,356 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.syvasoft.tallyfrontcrusher.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for TF_Vehicle_Rent
+ *  @author iDempiere (generated) 
+ *  @version Release 3.1
+ */
+@SuppressWarnings("all")
+public interface I_TF_Vehicle_Rent 
+{
+
+    /** TableName=TF_Vehicle_Rent */
+    public static final String Table_Name = "TF_Vehicle_Rent";
+
+    /** AD_Table_ID=1000170 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 1 - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(1);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Client.
+	  * Client/Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within client
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within client
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name C_ElementValue_ID */
+    public static final String COLUMNNAME_C_ElementValue_ID = "C_ElementValue_ID";
+
+	/** Set Profit Center.
+	  * Account Element
+	  */
+	public void setC_ElementValue_ID (int C_ElementValue_ID);
+
+	/** Get Profit Center.
+	  * Account Element
+	  */
+	public int getC_ElementValue_ID();
+
+	public org.compiere.model.I_C_ElementValue getC_ElementValue() throws RuntimeException;
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name CreditC_ElementValue_ID */
+    public static final String COLUMNNAME_CreditC_ElementValue_ID = "CreditC_ElementValue_ID";
+
+	/** Set Profit Center (Cr).
+	  * Credit Profit Center
+	  */
+	public void setCreditC_ElementValue_ID (int CreditC_ElementValue_ID);
+
+	/** Get Profit Center (Cr).
+	  * Credit Profit Center
+	  */
+	public int getCreditC_ElementValue_ID();
+
+	public org.compiere.model.I_C_ElementValue getCreditC_ElementValue() throws RuntimeException;
+
+    /** Column name DateAcct */
+    public static final String COLUMNNAME_DateAcct = "DateAcct";
+
+	/** Set Account Date.
+	  * Accounting Date
+	  */
+	public void setDateAcct (Timestamp DateAcct);
+
+	/** Get Account Date.
+	  * Accounting Date
+	  */
+	public Timestamp getDateAcct();
+
+    /** Column name DateFrom */
+    public static final String COLUMNNAME_DateFrom = "DateFrom";
+
+	/** Set Date From.
+	  * Starting date for a range
+	  */
+	public void setDateFrom (Timestamp DateFrom);
+
+	/** Get Date From.
+	  * Starting date for a range
+	  */
+	public Timestamp getDateFrom();
+
+    /** Column name DateTo */
+    public static final String COLUMNNAME_DateTo = "DateTo";
+
+	/** Set Date To.
+	  * End date of a date range
+	  */
+	public void setDateTo (Timestamp DateTo);
+
+	/** Get Date To.
+	  * End date of a date range
+	  */
+	public Timestamp getDateTo();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name DocStatus */
+    public static final String COLUMNNAME_DocStatus = "DocStatus";
+
+	/** Set Document Status.
+	  * The current status of the document
+	  */
+	public void setDocStatus (String DocStatus);
+
+	/** Get Document Status.
+	  * The current status of the document
+	  */
+	public String getDocStatus();
+
+    /** Column name DocumentNo */
+    public static final String COLUMNNAME_DocumentNo = "DocumentNo";
+
+	/** Set Document No.
+	  * Document sequence number of the document
+	  */
+	public void setDocumentNo (String DocumentNo);
+
+	/** Get Document No.
+	  * Document sequence number of the document
+	  */
+	public String getDocumentNo();
+
+    /** Column name GL_Journal_ID */
+    public static final String COLUMNNAME_GL_Journal_ID = "GL_Journal_ID";
+
+	/** Set Journal.
+	  * General Ledger Journal
+	  */
+	public void setGL_Journal_ID (int GL_Journal_ID);
+
+	/** Get Journal.
+	  * General Ledger Journal
+	  */
+	public int getGL_Journal_ID();
+
+	public org.compiere.model.I_GL_Journal getGL_Journal() throws RuntimeException;
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name IsCalculated */
+    public static final String COLUMNNAME_IsCalculated = "IsCalculated";
+
+	/** Set Calculated.
+	  * The value is calculated by the system
+	  */
+	public void setIsCalculated (boolean IsCalculated);
+
+	/** Get Calculated.
+	  * The value is calculated by the system
+	  */
+	public boolean isCalculated();
+
+    /** Column name Processed */
+    public static final String COLUMNNAME_Processed = "Processed";
+
+	/** Set Processed.
+	  * The document has been processed
+	  */
+	public void setProcessed (boolean Processed);
+
+	/** Get Processed.
+	  * The document has been processed
+	  */
+	public boolean isProcessed();
+
+    /** Column name Processing */
+    public static final String COLUMNNAME_Processing = "Processing";
+
+	/** Set Process Now	  */
+	public void setProcessing (boolean Processing);
+
+	/** Get Process Now	  */
+	public boolean isProcessing();
+
+    /** Column name Rent_Amt */
+    public static final String COLUMNNAME_Rent_Amt = "Rent_Amt";
+
+	/** Set Rent (Amount)	  */
+	public void setRent_Amt (BigDecimal Rent_Amt);
+
+	/** Get Rent (Amount)	  */
+	public BigDecimal getRent_Amt();
+
+    /** Column name Rented_Days */
+    public static final String COLUMNNAME_Rented_Days = "Rented_Days";
+
+	/** Set Rented Days	  */
+	public void setRented_Days (BigDecimal Rented_Days);
+
+	/** Get Rented Days	  */
+	public BigDecimal getRented_Days();
+
+    /** Column name Std_Days */
+    public static final String COLUMNNAME_Std_Days = "Std_Days";
+
+	/** Set Standard Days	  */
+	public void setStd_Days (BigDecimal Std_Days);
+
+	/** Get Standard Days	  */
+	public BigDecimal getStd_Days();
+
+    /** Column name Std_Rent */
+    public static final String COLUMNNAME_Std_Rent = "Std_Rent";
+
+	/** Set Standard Rent	  */
+	public void setStd_Rent (BigDecimal Std_Rent);
+
+	/** Get Standard Rent	  */
+	public BigDecimal getStd_Rent();
+
+    /** Column name TF_Quarry_ID */
+    public static final String COLUMNNAME_TF_Quarry_ID = "TF_Quarry_ID";
+
+	/** Set Quarry	  */
+	public void setTF_Quarry_ID (int TF_Quarry_ID);
+
+	/** Get Quarry	  */
+	public int getTF_Quarry_ID();
+
+	public I_TF_Quarry getTF_Quarry() throws RuntimeException;
+
+    /** Column name TF_Vehicle_Rent_ID */
+    public static final String COLUMNNAME_TF_Vehicle_Rent_ID = "TF_Vehicle_Rent_ID";
+
+	/** Set Vehicle Rent	  */
+	public void setTF_Vehicle_Rent_ID (int TF_Vehicle_Rent_ID);
+
+	/** Get Vehicle Rent	  */
+	public int getTF_Vehicle_Rent_ID();
+
+    /** Column name TF_Vehicle_Rent_UU */
+    public static final String COLUMNNAME_TF_Vehicle_Rent_UU = "TF_Vehicle_Rent_UU";
+
+	/** Set TF_Vehicle_Rent_UU	  */
+	public void setTF_Vehicle_Rent_UU (String TF_Vehicle_Rent_UU);
+
+	/** Get TF_Vehicle_Rent_UU	  */
+	public String getTF_Vehicle_Rent_UU();
+
+    /** Column name TF_VehicleType_ID */
+    public static final String COLUMNNAME_TF_VehicleType_ID = "TF_VehicleType_ID";
+
+	/** Set Vehicle Type	  */
+	public void setTF_VehicleType_ID (int TF_VehicleType_ID);
+
+	/** Get Vehicle Type	  */
+	public int getTF_VehicleType_ID();
+
+	public I_TF_VehicleType getTF_VehicleType() throws RuntimeException;
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+
+    /** Column name Vehicle_ID */
+    public static final String COLUMNNAME_Vehicle_ID = "Vehicle_ID";
+
+	/** Set Vehicle	  */
+	public void setVehicle_ID (int Vehicle_ID);
+
+	/** Get Vehicle	  */
+	public int getVehicle_ID();
+
+	public org.compiere.model.I_M_Product getVehicle() throws RuntimeException;
+}
