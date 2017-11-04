b9e8b87b698d0cd5e00ed08bec3a2b294560452e
Syed @ SyvaSoft <syed@syvasoft.com>
2017-11-04 16:43:49 +0530

    Bug in Inter Cash Book Entry function is fixed.

--- src/org/syvasoft/tallyfrontcrusher/callout/CalloutPayment_FromToBankAccount.java
+++ src/org/syvasoft/tallyfrontcrusher/callout/CalloutPayment_FromToBankAccount.java
@@ -3,8 +3,11 @@ package org.syvasoft.tallyfrontcrusher.callout;
 import java.util.Properties;
 
 import org.adempiere.base.IColumnCallout;
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
+import org.compiere.model.MUser;
+import org.compiere.util.Env;
 import org.syvasoft.tallyfrontcrusher.model.MGLPostingConfig;
 import org.syvasoft.tallyfrontcrusher.model.TF_MPayment;
 
@@ -21,7 +24,13 @@ public class CalloutPayment_FromToBankAccount implements IColumnCallout {
 			}
 		}
 		if(mTab.getValue(TF_MPayment.COLUMNNAME_FromTo_BankAccount_ID) != null) {
-			ElementValue_ID = MGLPostingConfig.getMGLPostingConfig(ctx).getInterBankInTransit_ID();			
+			ElementValue_ID = MGLPostingConfig.getMGLPostingConfig(ctx).getInterBankInTransit_ID();
+			if(ElementValue_ID == 0)
+				throw new AdempiereException("Inter Bank In Transit is not configured yet!");
+			mTab.setValue(TF_MPayment.COLUMNNAME_TF_BPartner_ID, 0);
+			MUser user = MUser.get(ctx, Env.getAD_User_ID(ctx));				
+			int bPartnerID = user.getC_BPartner_ID();
+			mTab.setValue(TF_MPayment.COLUMNNAME_C_BPartner_ID, bPartnerID);
 		}
 		mTab.setValue(TF_MPayment.COLUMNNAME_C_ElementValue_ID, ElementValue_ID);
 		return null;
