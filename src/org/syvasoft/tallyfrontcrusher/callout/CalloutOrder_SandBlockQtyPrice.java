f83ae00380281f0e10200cc7c15026c0dcb827e4
adempieresyed <adempieresyed@localhost>
2018-01-01 20:08:15 +0530

    Sand Block sales screen is customized to support permit, extra bucket & non permit sales.

--- src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrder_SandBlockQtyPrice.java
+++ src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrder_SandBlockQtyPrice.java
@@ -22,7 +22,7 @@ public class CalloutOrder_SandBlockQtyPrice implements IColumnCallout {
 		BigDecimal item1_Qty = BigDecimal.ZERO;
 		BigDecimal item2_Qty = BigDecimal.ZERO;
 		String orgType  = (String) mTab.getValue(TF_MOrder.COLUMNNAME_OrgType);
-		if(!orgType.equals(TF_MOrder.ORGTYPE_SandBlock))
+		if(!orgType.equals(TF_MOrder.ORGTYPE_SandBlock) || !mTab.getValueAsBoolean(TF_MOrder.COLUMNNAME_IsSOTrx))
 			return null;
 		
 		if(mTab.getValue(TF_MOrder.COLUMNNAME_Item1_BucketQty) != null)
