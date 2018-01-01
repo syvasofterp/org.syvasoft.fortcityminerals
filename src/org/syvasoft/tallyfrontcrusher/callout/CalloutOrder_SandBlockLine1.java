e40b04c24bf632cb917436d45e7b9983d505cfde
Syed @ SyvaSoft <syed@syvasoft.com>
2018-01-01 20:08:15 +0530

    Sand Block sales screen is customized to support permit, extra bucket & non permit sales.

--- src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrder_SandBlockLine1.java
+++ src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrder_SandBlockLine1.java
@@ -15,7 +15,8 @@ public class CalloutOrder_SandBlockLine1 implements IColumnCallout {
 	@Override
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
 		String orgType  = (String) mTab.getValue(TF_MOrder.COLUMNNAME_OrgType);
-		if(!orgType.equals(TF_MOrder.ORGTYPE_SandBlock))
+		
+		if(!orgType.equals(TF_MOrder.ORGTYPE_SandBlock) || !mTab.getValueAsBoolean(TF_MOrder.COLUMNNAME_IsSOTrx))
 			return null;
 		
 		if(mTab.getValue(TF_MOrder.COLUMNNAME_C_BPartner_ID) == null)
@@ -24,22 +25,34 @@ public class CalloutOrder_SandBlockLine1 implements IColumnCallout {
 		boolean isPermitSales = mTab.getValueAsBoolean(TF_MOrder.COLUMNNAME_Item1_IsPermitSales);
 		int orgID = (int) mTab.getValue(TF_MOrder.COLUMNNAME_AD_Org_ID) ;
 		if(isPermitSales) {
-			MSandBlockBucketConfig config = MSandBlockBucketConfig.getBucketConfig(orgID, MSandBlockBucketConfig.SANDTYPE_PermitSand);
-			mTab.setValue(TF_MOrder.COLUMNNAME_Item1_ID, config.getM_Product_ID());
-			mTab.setValue(TF_MOrder.COLUMNNAME_Item1_BucketRate, config.getSalesRatePerBucket());
-			mTab.setValue(TF_MOrder.COLUMNNAME_TonePerBucket, config.getSalesTonnagePerBucket());
-			
-			config = MSandBlockBucketConfig.getBucketConfig(orgID, MSandBlockBucketConfig.SANDTYPE_ExtraPermit);
-			mTab.setValue(TF_MOrder.COLUMNNAME_Item2_ID, config.getM_Product_ID());
-			mTab.setValue(TF_MOrder.COLUMNNAME_Item2_BucketRate, config.getSalesRatePerBucket());
-			mTab.setValue(TF_MOrder.COLUMNNAME_Item2_TonePerBucket, config.getSalesTonnagePerBucket());
-									
+			MSandBlockBucketConfig config;
+			if(!mField.getColumnName().equals(TF_MOrder.COLUMNNAME_Item2_BucketQty)) {
+				config = MSandBlockBucketConfig.getBucketConfig(orgID, MSandBlockBucketConfig.SANDTYPE_PermitSand);
+				mTab.setValue(TF_MOrder.COLUMNNAME_Item1_ID, config.getM_Product_ID());
+				mTab.setValue(TF_MOrder.COLUMNNAME_Item1_BucketRate, config.getSalesRatePerBucket());
+				mTab.setValue(TF_MOrder.COLUMNNAME_TonePerBucket, config.getSalesTonnagePerBucket());
+			}
+			BigDecimal item2_BucketQty = BigDecimal.ZERO;
+			if(mField.getColumnName().equals(TF_MOrder.COLUMNNAME_Item2_BucketQty)) {
+				if(mTab.getValue(TF_MOrder.COLUMNNAME_Item2_BucketQty) != null)
+					item2_BucketQty = (BigDecimal) mTab.getValue(TF_MOrder.COLUMNNAME_Item2_BucketQty);
+				if(item2_BucketQty.doubleValue() != 0) {
+					config = MSandBlockBucketConfig.getBucketConfig(orgID, MSandBlockBucketConfig.SANDTYPE_ExtraPermit);
+					mTab.setValue(TF_MOrder.COLUMNNAME_Item2_ID, config.getM_Product_ID());
+					mTab.setValue(TF_MOrder.COLUMNNAME_Item2_BucketRate, config.getSalesRatePerBucket());
+					mTab.setValue(TF_MOrder.COLUMNNAME_Item2_TonePerBucket, config.getSalesTonnagePerBucket());
+				}
+				else {
+					mTab.setValue(TF_MOrder.COLUMNNAME_Item2_ID, null);
+				}
+			}									
 		}
 		else {
 			MSandBlockBucketConfig config = MSandBlockBucketConfig.getBucketConfig(orgID, MSandBlockBucketConfig.SANDTYPE_WithoutPermit);
 			mTab.setValue(TF_MOrder.COLUMNNAME_Item1_ID, config.getM_Product_ID());
 			mTab.setValue(TF_MOrder.COLUMNNAME_Item1_BucketRate, config.getSalesRatePerBucket());
 			mTab.setValue(TF_MOrder.COLUMNNAME_TonePerBucket, config.getSalesTonnagePerBucket());
+			mTab.setValue(TF_MOrder.COLUMNNAME_Item2_ID, null);
 		}
 		//mTab.setValue(TF_MOrder.COLUMNNAME_Item2_ID, item1_id);
 		//mTab.setValue(TF_MOrder.COLUMNNAME_Item2_Tax_ID, item1_tax);
