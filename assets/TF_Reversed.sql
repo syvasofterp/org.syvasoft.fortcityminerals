7265e93b18f0778bfc586bc0ccf3f3ad198ec77f
adempieresyed <adempieresyed@localhost>
2018-03-23 22:06:30 +0530

    Reversal entries view created to filter fact acct records in Statement of account reports.

---
+++ assets/TF_Reversed.sql
@@ -0,0 +1,193 @@
+﻿
+-- DROP VIEW tf_reversed;
+
+CREATE OR REPLACE VIEW tf_reversed AS 
+ SELECT gl_journal.ad_client_id,
+    gl_journal.ad_org_id,
+    gl_journal.created,
+    gl_journal.createdby,
+    gl_journal.updated,
+    gl_journal.updatedby,
+    gl_journal.isactive,
+    gl_journal.documentno,
+    gl_journal.datedoc,
+    gl_journal.dateacct,
+    224 AS ad_table_id,
+    gl_journal.gl_journal_id AS record_id,
+    'N'::text AS issotrx,
+    gl_journal.posted,
+    gl_journal.processing,
+    gl_journal.processed,
+    gl_journal.docstatus,
+    gl_journal.processedon
+   FROM gl_journal
+  WHERE gl_journal.docstatus = 'RE'::bpchar
+UNION
+ SELECT c_invoice.ad_client_id,
+    c_invoice.ad_org_id,
+    c_invoice.created,
+    c_invoice.createdby,
+    c_invoice.updated,
+    c_invoice.updatedby,
+    c_invoice.isactive,
+    c_invoice.documentno,
+    c_invoice.dateinvoiced AS datedoc,
+    c_invoice.dateacct,
+    318 AS ad_table_id,
+    c_invoice.c_invoice_id AS record_id,
+    c_invoice.issotrx,
+    c_invoice.posted,
+    c_invoice.processing,
+    c_invoice.processed,
+    c_invoice.docstatus,
+    c_invoice.processedon
+   FROM c_invoice
+  WHERE c_invoice.docstatus = 'RE'
+UNION
+ SELECT m_inout.ad_client_id,
+    m_inout.ad_org_id,
+    m_inout.created,
+    m_inout.createdby,
+    m_inout.updated,
+    m_inout.updatedby,
+    m_inout.isactive,
+    m_inout.documentno,
+    m_inout.movementdate AS datedoc,
+    m_inout.dateacct,
+    319 AS ad_table_id,
+    m_inout.m_inout_id AS record_id,
+    m_inout.issotrx,
+    m_inout.posted,
+    m_inout.processing,
+    m_inout.processed,
+    m_inout.docstatus,
+    m_inout.processedon
+   FROM m_inout
+  WHERE m_inout.docstatus = 'RE'
+UNION
+ SELECT m_inventory.ad_client_id,
+    m_inventory.ad_org_id,
+    m_inventory.created,
+    m_inventory.createdby,
+    m_inventory.updated,
+    m_inventory.updatedby,
+    m_inventory.isactive,
+    m_inventory.documentno,
+    m_inventory.movementdate AS datedoc,
+    m_inventory.movementdate AS dateacct,
+    321 AS ad_table_id,
+    m_inventory.m_inventory_id AS record_id,
+    'N'::text AS issotrx,
+    m_inventory.posted,
+    m_inventory.processing,
+    m_inventory.processed,
+    m_inventory.docstatus,
+    m_inventory.processedon
+   FROM m_inventory
+  WHERE m_inventory.docstatus = 'RE'
+UNION
+ SELECT m_movement.ad_client_id,
+    m_movement.ad_org_id,
+    m_movement.created,
+    m_movement.createdby,
+    m_movement.updated,
+    m_movement.updatedby,
+    m_movement.isactive,
+    m_movement.documentno,
+    m_movement.movementdate AS datedoc,
+    m_movement.movementdate AS dateacct,
+    323 AS ad_table_id,
+    m_movement.m_movement_id AS record_id,
+    'N'::text AS issotrx,
+    m_movement.posted,
+    m_movement.processing,
+    m_movement.processed,
+    m_movement.docstatus,
+    m_movement.processedon
+   FROM m_movement
+  WHERE m_movement.docstatus = 'RE'
+UNION
+ SELECT m_production.ad_client_id,
+    m_production.ad_org_id,
+    m_production.created,
+    m_production.createdby,
+    m_production.updated,
+    m_production.updatedby,
+    m_production.isactive,
+    m_production.documentno,
+    m_production.movementdate AS datedoc,
+    m_production.movementdate AS dateacct,
+    325 AS ad_table_id,
+    m_production.m_production_id AS record_id,
+    'N'::text AS issotrx,
+    m_production.posted,
+    m_production.processing,
+    m_production.processed,
+    m_production.docstatus,
+    m_production.processedon
+   FROM m_production
+  WHERE m_production.docstatus = 'RE'
+UNION
+ SELECT c_payment.ad_client_id,
+    c_payment.ad_org_id,
+    c_payment.created,
+    c_payment.createdby,
+    c_payment.updated,
+    c_payment.updatedby,
+    c_payment.isactive,
+    c_payment.documentno,
+    c_payment.datetrx AS datedoc,
+    c_payment.dateacct,
+    335 AS ad_table_id,
+    c_payment.c_payment_id AS record_id,
+    'N'::text AS issotrx,
+    c_payment.posted,
+    c_payment.processing,
+    c_payment.processed,
+    c_payment.docstatus,
+    c_payment.processedon
+   FROM c_payment
+  WHERE c_payment.docstatus = 'RE'
+UNION
+ SELECT c_allocationhdr.ad_client_id,
+    c_allocationhdr.ad_org_id,
+    c_allocationhdr.created,
+    c_allocationhdr.createdby,
+    c_allocationhdr.updated,
+    c_allocationhdr.updatedby,
+    c_allocationhdr.isactive,
+    c_allocationhdr.documentno,
+    c_allocationhdr.datetrx AS datedoc,
+    c_allocationhdr.dateacct,
+    735 AS ad_table_id,
+    c_allocationhdr.c_allocationhdr_id AS record_id,
+    'N'::text AS issotrx,
+    c_allocationhdr.posted,
+    c_allocationhdr.processing,
+    c_allocationhdr.processed,
+    c_allocationhdr.docstatus,
+    c_allocationhdr.processedon
+   FROM c_allocationhdr
+  WHERE c_allocationhdr.docstatus = 'RE'
+UNION
+ SELECT c_bankstatement.ad_client_id,
+    c_bankstatement.ad_org_id,
+    c_bankstatement.created,
+    c_bankstatement.createdby,
+    c_bankstatement.updated,
+    c_bankstatement.updatedby,
+    c_bankstatement.isactive,
+    c_bankstatement.name AS documentno,
+    c_bankstatement.statementdate AS datedoc,
+    c_bankstatement.statementdate AS dateacct,
+    392 AS ad_table_id,
+    c_bankstatement.c_bankstatement_id AS record_id,
+    'N'::text AS issotrx,
+    c_bankstatement.posted,
+    c_bankstatement.processing,
+    c_bankstatement.processed,
+    c_bankstatement.docstatus,
+    c_bankstatement.processedon
+   FROM c_bankstatement
+  WHERE c_bankstatement.docstatus = 'RE'
+;
