a99082adf9737729c99b1efd4988de20087bddca
adempieresyed <adempieresyed@localhost>
2018-10-12 10:32:08 +0530

    lx commit

---
+++ src/org/syvasoft/tallyfrontcrusher/model/I_TF_Boulder_Receipt_LIne.java
@@ -0,0 +1,173 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.syvasoft.tallyfrontcrusher.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for TF_Boulder_Receipt_LIne
+ *  @author iDempiere (generated) 
+ *  @version Release 4.1
+ */
+@SuppressWarnings("all")
+public interface I_TF_Boulder_Receipt_LIne 
+{
+
+    /** TableName=TF_Boulder_Receipt_LIne */
+    public static final String Table_Name = "TF_Boulder_Receipt_LIne";
+
+    /** AD_Table_ID=1000188 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 1 - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(1);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Client.
+	  * Client/Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within client
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within client
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name Load */
+    public static final String COLUMNNAME_Load = "Load";
+
+	/** Set Load	  */
+	public void setLoad (int Load);
+
+	/** Get Load	  */
+	public int getLoad();
+
+    /** Column name TF_Boulder_Receipt_ID */
+    public static final String COLUMNNAME_TF_Boulder_Receipt_ID = "TF_Boulder_Receipt_ID";
+
+	/** Set Boulder Receipt	  */
+	public void setTF_Boulder_Receipt_ID (int TF_Boulder_Receipt_ID);
+
+	/** Get Boulder Receipt	  */
+	public int getTF_Boulder_Receipt_ID();
+
+	public I_TF_Boulder_Receipt getTF_Boulder_Receipt() throws RuntimeException;
+
+    /** Column name TF_Boulder_Receipt_LIne_ID */
+    public static final String COLUMNNAME_TF_Boulder_Receipt_LIne_ID = "TF_Boulder_Receipt_LIne_ID";
+
+	/** Set Boulder Receipt Line	  */
+	public void setTF_Boulder_Receipt_LIne_ID (int TF_Boulder_Receipt_LIne_ID);
+
+	/** Get Boulder Receipt Line	  */
+	public int getTF_Boulder_Receipt_LIne_ID();
+
+    /** Column name TF_Boulder_Receipt_LIne_UU */
+    public static final String COLUMNNAME_TF_Boulder_Receipt_LIne_UU = "TF_Boulder_Receipt_LIne_UU";
+
+	/** Set TF_Boulder_Receipt_LIne_UU	  */
+	public void setTF_Boulder_Receipt_LIne_UU (String TF_Boulder_Receipt_LIne_UU);
+
+	/** Get TF_Boulder_Receipt_LIne_UU	  */
+	public String getTF_Boulder_Receipt_LIne_UU();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+
+    /** Column name Vehicle_ID */
+    public static final String COLUMNNAME_Vehicle_ID = "Vehicle_ID";
+
+	/** Set Vehicle	  */
+	public void setVehicle_ID (int Vehicle_ID);
+
+	/** Get Vehicle	  */
+	public int getVehicle_ID();
+
+	public org.compiere.model.I_M_Product getVehicle() throws RuntimeException;
+}
