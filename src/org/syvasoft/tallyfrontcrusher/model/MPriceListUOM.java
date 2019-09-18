e6b340a9d25da849232044e46347e984bb740507
Syed @ SyvaSoft <syed@syvasoft.com>
2019-09-18 13:50:35 +0530

    Price Uniqueness check for UOM Based Price

--- src/org/syvasoft/tallyfrontcrusher/model/MPriceListUOM.java
+++ src/org/syvasoft/tallyfrontcrusher/model/MPriceListUOM.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.Query;
+import org.compiere.util.DB;
 
 public class MPriceListUOM extends X_TF_PriceListUOM {
 
@@ -23,6 +25,33 @@ public class MPriceListUOM extends X_TF_PriceListUOM {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	protected boolean beforeSave(boolean newRecord) {
+		
+		validateUniqueness(newRecord);		
+		return super.beforeSave(newRecord);
+	}
+	
+	public void validateUniqueness(boolean newRecord) {
+		String sql = "SELECT COUNT(*) FROM TF_PriceListUOM WHERE M_Product_ID = ? AND "
+				+ " C_UOM_ID = ? AND IsSOTrx = ? AND COALESCE(C_BPartner_ID,0) = ? " ;
+		if(!newRecord) {
+			sql += " AND TF_PriceListUOM_ID != ?";
+		}
+		int count = 0;
+		if(newRecord) {
+			count = DB.getSQLValue(get_TrxName(), sql, getM_Product_ID(), getC_UOM_ID(), 
+					isSOTrx() ? "Y" : "N", getC_BPartner_ID());
+		}
+		else {
+			count = DB.getSQLValue(get_TrxName(), sql, getM_Product_ID(), getC_UOM_ID(), 
+					isSOTrx() ? "Y" : "N", getC_BPartner_ID(), getTF_PriceListUOM_ID());
+		}
+		if(count > 0) {
+			throw new AdempiereException("Price is already entered for the current Product, UOM and Business Partner!");
+		}
+	}
+	
 	public static BigDecimal getPrice(Properties ctx, int M_Product_ID, int C_UOM_ID, 
 			int C_BPartner_ID, boolean isSOTrx) {
 		String whereClause = "M_Product_ID = ? AND C_UOM_ID = ? AND IsSOTrx=? "
