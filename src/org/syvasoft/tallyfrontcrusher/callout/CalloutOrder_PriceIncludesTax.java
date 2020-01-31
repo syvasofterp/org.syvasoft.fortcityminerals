06b48662f5439d8bff32e990031f5bbfa02d9d59
balamuruugan <balamuruugan@gmail.com>
2020-01-31 09:09:12 +0530

    Null Exception Issue has been fixed in Purchase Entry Screen

--- src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrder_PriceIncludesTax.java
+++ src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrder_PriceIncludesTax.java
@@ -24,41 +24,42 @@ public class CalloutOrder_PriceIncludesTax implements IColumnCallout {
 	@Override
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {		
 		if(value != null){
-			
-			int bPartner_ID = 0;
-			int product_ID = 0;
-			int vehicle_ID = 0;
-			boolean isTaxIncluded = false;
-			if(mTab.getValue(TF_MOrder.COLUMNNAME_C_BPartner_ID) != null)
-				bPartner_ID	= (int) mTab.getValue(TF_MOrder.COLUMNNAME_C_BPartner_ID);
-			
-			if(mTab.getValue(TF_MOrder.COLUMNNAME_Item1_ID) != null)
-				product_ID = (int) mTab.getValue(TF_MOrder.COLUMNNAME_Item1_ID);
-			
-			if(mTab.getValue(TF_MOrder.COLUMNNAME_IsTaxIncluded1) != null)
-				isTaxIncluded = (boolean) mTab.getValue(TF_MOrder.COLUMNNAME_IsTaxIncluded1);
-			
-			if(mTab.getValue(TF_MOrder.COLUMNNAME_TF_RentedVehicle_ID) != null)
-				vehicle_ID = (int) mTab.getValue(TF_MOrder.COLUMNNAME_TF_RentedVehicle_ID);
-			
-			
-			TF_MProduct product = new TF_MProduct(ctx, product_ID, null);
-			int tax_ID = product.getTax_ID(isTaxIncluded);
-			
-			if(mField.getColumnName().equals(TF_MOrder.COLUMNNAME_C_BPartner_ID) ||
-					mField.getColumnName().equals(TF_MOrder.COLUMNNAME_Item1_ID) || 
-					mField.getColumnName().equals(TF_MOrder.COLUMNNAME_IsTaxIncluded1)) {
-					mTab.setValue(TF_MOrder.COLUMNNAME_Item1_Tax_ID, tax_ID);
-			}
-			
-			if(vehicle_ID > 0) {
-				MRentedVehicle rentedVehicle = new MRentedVehicle(ctx, vehicle_ID, null);
+			if((boolean)mTab.getValue(TF_MOrder.COLUMNNAME_IsSOTrx) == true) {
+				int bPartner_ID = 0;
+				int product_ID = 0;
+				int vehicle_ID = 0;
+				boolean isTaxIncluded = false;
+				if(mTab.getValue(TF_MOrder.COLUMNNAME_C_BPartner_ID) != null)
+					bPartner_ID	= (int) mTab.getValue(TF_MOrder.COLUMNNAME_C_BPartner_ID);
 				
-				if(rentedVehicle != null){
-					TF_MProduct vehicle = new TF_MProduct(ctx, rentedVehicle.getM_Product_ID(), null);
-					int vehicle_tax_ID = vehicle.getTax_ID(isTaxIncluded);
+				if(mTab.getValue(TF_MOrder.COLUMNNAME_Item1_ID) != null)
+					product_ID = (int) mTab.getValue(TF_MOrder.COLUMNNAME_Item1_ID);
+				
+				if(mTab.getValue(TF_MOrder.COLUMNNAME_IsTaxIncluded1) != null)
+					isTaxIncluded = (boolean) mTab.getValue(TF_MOrder.COLUMNNAME_IsTaxIncluded1);
+				
+				if(mTab.getValue(TF_MOrder.COLUMNNAME_TF_RentedVehicle_ID) != null)
+					vehicle_ID = (int) mTab.getValue(TF_MOrder.COLUMNNAME_TF_RentedVehicle_ID);
+				
+				
+				TF_MProduct product = new TF_MProduct(ctx, product_ID, null);
+				int tax_ID = product.getTax_ID(isTaxIncluded);
+				
+				if(mField.getColumnName().equals(TF_MOrder.COLUMNNAME_C_BPartner_ID) ||
+						mField.getColumnName().equals(TF_MOrder.COLUMNNAME_Item1_ID) || 
+						mField.getColumnName().equals(TF_MOrder.COLUMNNAME_IsTaxIncluded1)) {
+						mTab.setValue(TF_MOrder.COLUMNNAME_Item1_Tax_ID, tax_ID);
+				}
+				
+				if(vehicle_ID > 0) {
+					MRentedVehicle rentedVehicle = new MRentedVehicle(ctx, vehicle_ID, null);
 					
-					mTab.setValue(TF_MOrder.COLUMNNAME_Rent_Tax_ID, vehicle_tax_ID);
+					if(rentedVehicle != null){
+						TF_MProduct vehicle = new TF_MProduct(ctx, rentedVehicle.getM_Product_ID(), null);
+						int vehicle_tax_ID = vehicle.getTax_ID(isTaxIncluded);
+						
+						mTab.setValue(TF_MOrder.COLUMNNAME_Rent_Tax_ID, vehicle_tax_ID);
+					}
 				}
 			}
 		}
