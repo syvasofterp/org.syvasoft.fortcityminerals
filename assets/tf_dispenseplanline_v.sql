791f67fffa595bffd2321452b6d5a8ecd995d60b
Syed <syed@syvasoft.com>
2021-05-19 16:44:32 +0530

    view updated

--- assets/tf_dispenseplanline_v.sql
+++ assets/tf_dispenseplanline_v.sql
@@ -1,33 +1,55 @@
-DROP VIEW IF EXISTS adempiere.tf_transporterdc_v;
+-- View: adempiere.tf_dispenseplanline_v
 
-CREATE OR REPLACE VIEW adempiere.tf_transporterdc_v AS
- SELECT io.ad_client_id,
-    io.ad_org_id,
-    io.m_inout_id,
-    io.documentno AS shipmentno,
-    io.docstatus,
-    iol.docstatus receiptlinestatus,
-    io.movementdate,
-    w.documentno AS dcno,
-    io.c_bpartner_id AS vendor_id,
-    iol.tf_lumpsumrent_config_id,
-    iol.tf_destination_id,
-    w.c_bpartner_id,
-    w.m_product_id,
-    w.tf_rentedvehicle_id,
-    iol.c_uom_id,
-    iol.price,
-    rent.rentmargin,
-    io.created,
-    io.createdby,
-    iol.updated,
-    iol.updatedby,
-    io.isactive,
-    319 AS ad_table_id,
-    io.m_inout_id AS record_id
-   
-FROM m_inout io JOIN m_inoutline iol ON iol.m_inout_id = io.m_inout_id
-     JOIN tf_weighmententry w ON w.tf_weighmententry_id = io.tf_weighmententry_id
-     LEFT JOIN tf_lumpsumrent_config rent ON rent.tf_lumpsumrent_config_id = iol.tf_lumpsumrent_config_id
-  WHERE io.issotrx = 'N';
+-- DROP VIEW adempiere.tf_dispenseplanline_v;
+
+CREATE OR REPLACE VIEW adempiere.tf_dispenseplanline_v
+ AS
+ SELECT dl.tf_dispenseplanline_id,
+    dl.scheduledate,
+    dl.ad_client_id,
+    dl.ad_org_id,
+    dl.isactive,
+    dl.created,
+    dl.createdby,
+    dl.updated,
+    dl.updatedby,
+    dl.documentno,
+    dl.dateordered,
+    dl.c_orderline_id,
+    dl.c_bpartner_id,
+    dl.paymentrule,
+    dl.m_product_id,
+    dl.dispenseqty,
+    dl.delivereddpqty,
+    dl.balancedpqty,
+    dl.c_uom_id,
+    dl.tf_destination_id,
+    dl.isroyaltypassinclusive,
+    dl.isrentinclusive,
+    dl.istaxincluded,
+    dl.priceentered,
+    dl.ispriceconfidential,
+    dl.deliverycontact,
+    dl.shipmentto,
+    dl.shipmentdestination,
+    dl.priority,
+    dl.type,
+    dl.origindate,
+    dl.overunitdelivery,
+    dl.allowcarryforward,
+    dl.docstatus,
+        CASE
+            WHEN trunc(d.scheduledate) < trunc(getdate()) AND (dl.docstatus::text = ANY (ARRAY['RV'::character varying::text, 'IP'::character varying::text])) THEN 'Red'::text
+            WHEN dl.docstatus::text = 'CO'::text THEN 'Green'::text
+            ELSE 'Black'::text
+        END AS color,
+    d.tf_dispenseplan_id,
+    1000334 AS ad_table_id,
+    dl.tf_dispenseplanline_id AS record_id
+   FROM tf_dispenseplanline dl
+     JOIN tf_dispenseplan d ON d.tf_dispenseplan_id = dl.tf_dispenseplan_id
+  WHERE (dl.docstatus::text = ANY (ARRAY['IP'::character varying::text, 'RV'::character varying::text])) AND (dl.allowcarryforward = 'Y'::bpchar OR trunc(dl.scheduledate) = trunc(getdate())) OR dl.docstatus::text = 'CO'::text AND (trunc(dl.scheduledate) = trunc(getdate()));
+
+ALTER TABLE adempiere.tf_dispenseplanline_v
+    OWNER TO adempiere;
 
--- assets/tf_transporterdc_v.sql
+++ assets/tf_transporterdc_v.sql
@@ -1,31 +1,22 @@
-/***/DROP VIEW adempiere.tf_transporterdc_v;
+DROP VIEW IF EXISTS adempiere.tf_transporterdc_v;
 
-/***/CREATE  VIEW adempiere.tf_transporterdc_v
- AS
- /***/SELECT io.ad_client_id,
+CREATE OR REPLACE VIEW adempiere.tf_transporterdc_v AS
+ SELECT io.ad_client_id,
     io.ad_org_id,
     io.m_inout_id,
-	iol.m_inoutline_id,
     io.documentno AS shipmentno,
     io.docstatus,
-    iol.docstatus AS receiptlinestatus,
+    iol.docstatus receiptlinestatus,
     io.movementdate,
     w.documentno AS dcno,
     io.c_bpartner_id AS vendor_id,
     iol.tf_lumpsumrent_config_id,
     iol.tf_destination_id,
-    iol.distance,
-    iol.qtyentered,
     w.c_bpartner_id,
     w.m_product_id,
     w.tf_rentedvehicle_id,
     iol.c_uom_id,
-    u.name AS uomname,
     iol.price,
-        CASE
-            WHEN iol.c_uom_id = 1000081::numeric THEN iol.distance * iol.qtyentered * iol.price
-            ELSE iol.qtyentered * iol.price
-        END AS totalamt,
     rent.rentmargin,
     io.created,
     io.createdby,
@@ -34,9 +25,9 @@
     io.isactive,
     319 AS ad_table_id,
     io.m_inout_id AS record_id
-   FROM m_inout io
-     JOIN m_inoutline iol ON iol.m_inout_id = io.m_inout_id
+   
+FROM m_inout io JOIN m_inoutline iol ON iol.m_inout_id = io.m_inout_id
      JOIN tf_weighmententry w ON w.tf_weighmententry_id = io.tf_weighmententry_id
      LEFT JOIN tf_lumpsumrent_config rent ON rent.tf_lumpsumrent_config_id = iol.tf_lumpsumrent_config_id
-     LEFT JOIN c_uom u ON u.c_uom_id = iol.c_uom_id
-  WHERE io.issotrx = 'N'::bpchar AND io.docstatus <> 'RE'::bpchar;
+  WHERE io.issotrx = 'N';
+
